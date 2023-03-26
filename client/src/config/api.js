2bb14df522de1c6352c61357ea5606af5be153bb
wlgpro <wlingipro@gmail.com>
2023-03-27 05:46:33 +0700

    deploy

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create base URL API
 export const API = axios.create({
   // baseURL: "http://localhost:5000/api/v1/",
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "http://hallo-corona-production.up.railway.app/api/v1/",
 });
 
 // Set Authorization Token Header
